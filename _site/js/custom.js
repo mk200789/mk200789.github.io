e15aab74c92f666e6ef6358fd52b87cbff81becb
Kim Mok <cafe.mui@gmail.com>
2017-09-17 15:37:38 -0500

    include tags in projects and added another project

--- _site/js/custom.js
+++ _site/js/custom.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
      console.log("ready");
+     loadTags();
+
      $('.custom-menu-link').bind('click', function(event){
           console.log("menu link clicked");
           var menu = document.getElementById("menu");
@@ -9,6 +11,44 @@ $(document).ready(function(){
           var menu_horizontal = document.getElementById("menu-horizontal");
           menu_horizontal.setAttribute("class", "pure-menu pure-menu-horizontal custom-can-transform custom-menu")
      });
+
+     function loadTags(){
+          var tags = document.getElementsByClassName("tag");
+
+          for (var i=0; i<tags.length; i++){
+               colorTag(tags[i].innerHTML.trim().toLowerCase(), tags[i].style);
+          }
+     }
+
+     function colorTag(tag, tagStyle){
+          switch(tag){
+               case 'core data':
+                    tagStyle.backgroundColor='#33bfc7';
+                    break;
+               case 'swift':
+                    tagStyle.backgroundColor='#e6d928';
+                    break;
+               case 'javascript':
+               case 'html5':
+                    tagStyle.backgroundColor='#dade26';
+                    break;
+               case 'python':
+               case 'django':
+                    tagStyle.backgroundColor='#dc8b8b';
+                    break;
+               case 'game':
+               case 'compiler':
+               case 'mobile':
+               case 'alexa':
+               case 'quiz':
+                    tagStyle.backgroundColor='#ac72af';
+                    break;
+               default:
+                    tagStyle.backgroundColor='#8787a9';
+
+          }
+     }
+
 
      $('.type-it').typeIt({
          speed: 70,
